bb9ad947b3a8edc145ac296ef719596acd04d6a5
bonacciog <giovanni.bonaccio96@gmail.com>
2018-12-03 18:34:05 +0100

    Aggiunta funzionalita aggiungi giocatore e elimina giocatore. Iniziata inoltre generazione random

--- Perudo_Datapath.vhd
+++ Perudo_Datapath.vhd
@@ -6,18 +6,45 @@ use work.perudo_package.all;
 entity Perudo_Datapath is
 	port
 	(
-			CLOCK			:	in		std_logic;
-			RESET_N		: 	in 	std_logic;
+			CLOCK											:	in		std_logic;
+			RESET_N										: 	in 	std_logic;
 			
 			-- Connections for the Controller
-			... Le decideremo tra un po’ quando le cose saranno più chiare
+			NUOVO_GIOCATORE							:	in		std_logic;
+			NUMERO_PER_GENERAZIONE_CASUALE		:	in		integer range MIN_NUMERO_PER_GENERAZIONE_CASUALE to MAX_NUMERO_PER_GENERAZIONE_CASUALE
+			ELIMINA_GIOCATORE 						:	in		std_logic;
+			
 			-- Connections for the View
-			... Idem
 	);
 end entity;
 
 architecture RTL of Perudo_Datapath is
-	signal giocatori_in_campo			: giocatore_array(MIN_GIOCATORI to MAX_GIOCATORI);
-begin
 
+	signal giocatori_in_campo			: giocatore_array(0 to MAX_GIOCATORI-1);
+	signal numero_giocatori_in_campo	: integer range 0 to MAX_GIOCATORI-1;
+	
+begin
+	
+	EliminaGiocatore_RTL : process(ELIMINA_GIOCATORE)
+	begin
+		
+		for i in 0 to MAX_DADI-1 loop
+			giocatori_in_campo(numero_giocatori_in_campo).dadi_in_mano(i) <=	NOP;
+		end loop;
+		
+		numero_giocatori_in_campo <= numero_giocatori_in_campo - 1;
+		
+	end process;
+	
+	
+	NuovoGiocatore_RTL : process(NUOVO_GIOCATORE)
+	begin
+		
+		for i in 0 to MAX_DADI-1 loop
+			giocatori_in_campo(numero_giocatori_in_campo).dadi_in_mano(i) <=	scegli_dado_casuale(NUMERO_PER_GENERAZIONE_CASUALE);
+		end loop;
+		
+		numero_giocatori_in_campo <= numero_giocatori_in_campo + 1;
+		
+	end process;
 end architecture;
